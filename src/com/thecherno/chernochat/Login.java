68b2ba2fbcb2d9642bebbe654ba8d063087f8c09
Yan Chernikov <y@nchernikov.com>
2013-07-30 23:33:27 +0200

    Episode 2

--- src/com/thecherno/chernochat/Login.java
+++ src/com/thecherno/chernochat/Login.java
@@ -1,24 +1,38 @@
 package com.thecherno.chernochat;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class Login extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	private JPanel contentPane;
+	private JTextField txtName;
 
 	public Login() {
+		setResizable(false);
+		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setSize(300, 380);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtName = new JTextField();
+		txtName.setBounds(67, 50, 165, 28);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblName = new JLabel("Name:");
+		lblName.setBounds(127, 34, 45, 16);
+		contentPane.add(lblName);
 	}
 
 	public static void main(String[] args) {
@@ -33,5 +47,5 @@ public class Login extends JFrame {
 			}
 		});
 	}
-
 }
+
