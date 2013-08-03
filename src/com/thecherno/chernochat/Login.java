b2e9e5bc625d6bc8dfef414af9e316d7101b3e74
Mr Cherno <y@nchernikov.com>
2013-08-04 01:08:44 +0200

    Episode 4

--- src/com/thecherno/chernochat/Login.java
+++ src/com/thecherno/chernochat/Login.java
@@ -1,6 +1,8 @@
 package com.thecherno.chernochat;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,8 +17,8 @@ public class Login extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtName;
 	private JTextField txtAddress;
-	private JLabel lblIpAddress;
 	private JTextField txtPort;
+	private JLabel lblIpAddress;
 	private JLabel lblPort;
 	private JLabel lblAddressDesc;
 	private JLabel lblPortDesc;
@@ -31,21 +33,21 @@ public class Login extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		txtName = new JTextField();
 		txtName.setBounds(67, 50, 165, 28);
 		contentPane.add(txtName);
 		txtName.setColumns(10);
-		
+
 		JLabel lblName = new JLabel("Name:");
 		lblName.setBounds(127, 34, 45, 16);
 		contentPane.add(lblName);
-		
+
 		txtAddress = new JTextField();
 		txtAddress.setBounds(67, 116, 165, 28);
 		contentPane.add(txtAddress);
 		txtAddress.setColumns(10);
-		
+
 		lblIpAddress = new JLabel("IP Address:");
 		lblIpAddress.setBounds(111, 96, 77, 16);
 		contentPane.add(lblIpAddress);
@@ -54,22 +56,35 @@ public class Login extends JFrame {
 		txtPort.setColumns(10);
 		txtPort.setBounds(67, 191, 165, 28);
 		contentPane.add(txtPort);
-		
+
 		lblPort = new JLabel("Port:");
 		lblPort.setBounds(133, 171, 34, 16);
 		contentPane.add(lblPort);
-		
+
 		lblAddressDesc = new JLabel("(eg. 192.168.0.2)");
 		lblAddressDesc.setBounds(94, 142, 112, 16);
 		contentPane.add(lblAddressDesc);
-		
+
 		lblPortDesc = new JLabel("(eg. 8192)");
 		lblPortDesc.setBounds(116, 218, 68, 16);
 		contentPane.add(lblPortDesc);
-		
+
 		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = txtName.getText();
+				String address = txtAddress.getText();
+				int port = Integer.parseInt(txtPort.getText());
+				login(name, address, port);
+			}
+		});
 		btnLogin.setBounds(91, 311, 117, 29);
 		contentPane.add(btnLogin);
+	}
+
+	private void login(String name, String address, int port) {
+		dispose();
+		System.out.println(name + ", " + address + ", " + port);
 	}
 
 	public static void main(String[] args) {
@@ -85,4 +100,3 @@ public class Login extends JFrame {
 		});
 	}
 }
-
